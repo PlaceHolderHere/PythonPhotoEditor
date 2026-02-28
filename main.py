7086940db7c249dbd4c3473c40569dfd00eccc4a
PlaceHolderHere <holderhereinplace@gmail.com>
2026-02-28 23:52:28 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from PIL import Image
+
+
+def gray_scale(image):
+    pass
+
+
+def edge_detection(image):
+    pass
+
+
+def remove_background(image):
+    pass
+
+
+def flip_image(image):
+    pass
+
+
+def blur_image(image):
+    pass
+
+
