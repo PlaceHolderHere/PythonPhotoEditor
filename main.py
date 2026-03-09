1293275b6842c4b665e616b48a5e67dac3d0c8de
PlaceHolderHere <holderhereinplace@gmail.com>
2026-03-09 17:21:11 +0800

    Added a blur image function

--- main.py
+++ main.py
@@ -17,17 +17,18 @@ def flip_image(image):
     pass
 
 
-def blur_image(image):
-    pass
+def blur_image(image, intensity):  # Base Level is 2
+    return image.filter(ImageFilter.GaussianBlur(radius=intensity))
 
 
 def main():
     image_path = "input/test.JPG"
-    output_name = "edges.JPG"
+    output_name = "blur.JPG"
     output_path = f"output/{output_name}"
     image = Image.open(image_path)
     # output = gray_scale(image)  # Testing
-    output = edge_detection(image)  # Testing
+    # output = edge_detection(image)  # Testing
+    output = blur_image(image, 5)  # Testing
     output.save(output_path)
 
 
