470e450b5dbba7bc4ca0ddf89da2b01277bcbfad
PlaceHolderHere <holderhereinplace@gmail.com>
2026-03-31 20:32:11 +0800

    Added a new file called GUI.py for a gui version of the image editor made with customtkinter

---
+++ GUI.py
@@ -0,0 +1,11 @@
+import image_functions
+import customtkinter
+
+# Initialization
+customtkinter.set_appearance_mode("System   ")
+customtkinter.set_default_color_theme("dark-blue")
+root = customtkinter.CTk()
+
+root.title("Python Image Editor")
+
+root.mainloop()
