f37de15d93d87448e55b9015ba47a787c4063607
PlaceHolderHere <holderhereinplace@gmail.com>
2026-06-01 23:36:06 +0800

    Added an upload button with placeholder text that updates the display image, note the comment on Line 19

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
 import image_functions
+import os
 import tkinter as tk
+from tkinter import filedialog
 
 # Constants
 SCREEN_WIDTH = 1280
@@ -9,6 +11,14 @@ ACCENT_COLOR = "#26384D"
 SIDEBAR_WIDTH = 88
 NAVBAR_HEIGHT = 64
 PLACEHOLDER_PHOTO = "assets/placeholder-photo.png"
+VALID_FILE_EXTENSIONS = {".png": 0, ".pgm": 0, ".ppm": 0, ".gif": 0}
+
+# Formatting a string for filedialog option
+UPLOAD_BUTTON_EXTENSIONS = "*.png *.pgm *.ppm *.gif"
+
+# NOTE:
+# Currently PhotoImage in tkinter only supports 4 file types, however PIL supports a lot more
+# In the future I'll convert the inputted image into a png for display purposes and store it in ./assets
 
 class PythonImageEditor:
     def __init__(self):
@@ -25,6 +35,9 @@ class PythonImageEditor:
         self.navbar_frame = tk.Frame(self.root, bg=ACCENT_COLOR, width=SCREEN_WIDTH, height=NAVBAR_HEIGHT)
         self.navbar_frame.pack(side="top", padx=0, pady=0, fill="x")
 
+        self.upload_button = tk.Button(self.navbar_frame, text="upload", command=self.upload_function)
+        self.upload_button.pack(side="left", padx=16, pady=20)
+
         # SideBar
         self.sidebar_frame = tk.Frame(self.root, bg=PRIMARY_COLOR, width=SIDEBAR_WIDTH, height=SCREEN_HEIGHT)
         self.sidebar_frame.pack(side="left")
@@ -40,6 +53,45 @@ class PythonImageEditor:
 
         # Main Loop
         self.root.mainloop()
+
+    def upload_function(self):
+        selected_path = filedialog.askopenfilename(
+            title="Select a Photo to Edit",
+            filetypes=[("Photos", UPLOAD_BUTTON_EXTENSIONS)],
+        )
+
+        # Checking if a file was selected
+        if selected_path:
+            if self.verify_upload(selected_path):
+                # Updating Related Variables/Labels After Selecting a new Photo
+                self.photo_path = selected_path
+                self.photo = tk.PhotoImage(file=self.photo_path)
+                self.photo_label.config(image=self.photo)
+            else:
+                # Creating a pop-up to show an error
+                popup = tk.Toplevel(self.root)
+                popup.title("Error")
+                popup.geometry("250x75")
+                popup.resizable(False, False)
+                popup.grab_set() # blocks user from performing other actions unless pop-up is closed
+                self.root.eval(f"tk::PlaceWindow {popup} center") # centers popup
+
+                # Error Message
+                label = tk.Label(popup, text="Invalid Input! Please try again.", font=("Arial", 12), pady=20)
+                label.pack()
+
+                # Closes the Popup after 2000 ms
+                popup.after(2000, popup.destroy)
+
+    @staticmethod
+    def verify_upload(path):
+        if not os.path.exists(path):
+            return False
+
+        name, ext = os.path.splitext(path)
+        if ext.lower() not in VALID_FILE_EXTENSIONS:
+            return False
+        return True
 
 
 if __name__ == "__main__":
